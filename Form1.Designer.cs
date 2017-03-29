93d03ea309662670c5ab31868e760d0e4b63abaf
Yuri Cordeiro Teixeira <yuri.c.teixeira@hotmail.com>
2017-03-29 00:48:58 -0300

    Trabalho de CG
    Processamento de imagem

---
+++ Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace Trabalho
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.btnToTransform = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.ptbImage = new System.Windows.Forms.PictureBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.ptbTransformedImage = new System.Windows.Forms.PictureBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.ptbImage2 = new System.Windows.Forms.PictureBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart3 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbImage)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbTransformedImage)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbImage2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnToTransform
+            // 
+            this.btnToTransform.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnToTransform.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnToTransform.Location = new System.Drawing.Point(5, 263);
+            this.btnToTransform.Name = "btnToTransform";
+            this.btnToTransform.Size = new System.Drawing.Size(182, 31);
+            this.btnToTransform.TabIndex = 1;
+            this.btnToTransform.Text = "Transform";
+            this.btnToTransform.UseVisualStyleBackColor = true;
+            this.btnToTransform.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.ptbImage);
+            this.groupBox1.Location = new System.Drawing.Point(5, 7);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(182, 215);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Home Image";
+            // 
+            // ptbImage
+            // 
+            this.ptbImage.Image = global::Trabalho.Properties.Resources.Imagem1;
+            this.ptbImage.Location = new System.Drawing.Point(7, 19);
+            this.ptbImage.Name = "ptbImage";
+            this.ptbImage.Size = new System.Drawing.Size(169, 188);
+            this.ptbImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptbImage.TabIndex = 0;
+            this.ptbImage.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.ptbTransformedImage);
+            this.groupBox2.Location = new System.Drawing.Point(381, 7);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(182, 215);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Transformed Image";
+            // 
+            // ptbTransformedImage
+            // 
+            this.ptbTransformedImage.Location = new System.Drawing.Point(7, 19);
+            this.ptbTransformedImage.Name = "ptbTransformedImage";
+            this.ptbTransformedImage.Size = new System.Drawing.Size(169, 188);
+            this.ptbTransformedImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptbTransformedImage.TabIndex = 0;
+            this.ptbTransformedImage.TabStop = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Negativ Image"});
+            this.comboBox1.Location = new System.Drawing.Point(5, 228);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(182, 21);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.ptbImage2);
+            this.groupBox3.Location = new System.Drawing.Point(193, 7);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(182, 215);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Home Image 1";
+            // 
+            // ptbImage2
+            // 
+            this.ptbImage2.Image = global::Trabalho.Properties.Resources.Imagem2;
+            this.ptbImage2.Location = new System.Drawing.Point(7, 19);
+            this.ptbImage2.Name = "ptbImage2";
+            this.ptbImage2.Size = new System.Drawing.Size(169, 188);
+            this.ptbImage2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptbImage2.TabIndex = 0;
+            this.ptbImage2.TabStop = false;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(569, 7);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Histogram R";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(306, 215);
+            this.chart1.TabIndex = 6;
+            this.chart1.Text = "Histogram R";
+            // 
+            // chart2
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart2.Legends.Add(legend2);
+            this.chart2.Location = new System.Drawing.Point(569, 228);
+            this.chart2.Name = "chart2";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Histogram G";
+            this.chart2.Series.Add(series2);
+            this.chart2.Size = new System.Drawing.Size(306, 215);
+            this.chart2.TabIndex = 7;
+            this.chart2.Text = "Histogram G";
+            // 
+            // chart3
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart3.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart3.Legends.Add(legend3);
+            this.chart3.Location = new System.Drawing.Point(877, 7);
+            this.chart3.Name = "chart3";
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Histogram B";
+            this.chart3.Series.Add(series3);
+            this.chart3.Size = new System.Drawing.Size(306, 215);
+            this.chart3.TabIndex = 8;
+            this.chart3.Text = "Histogram B";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1188, 453);
+            this.Controls.Add(this.chart3);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnToTransform);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Trabalho - Computação Gráfica";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.ptbImage)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.ptbTransformedImage)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.ptbImage2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ptbImage;
+        private System.Windows.Forms.Button btnToTransform;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.PictureBox ptbTransformedImage;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.PictureBox ptbImage2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,431 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization;
+
+namespace Trabalho
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Bitmap image = (Bitmap)ptbImage.Image;
+            Bitmap image2 = (Bitmap)ptbImage2.Image;
+            ptbTransformedImage.Image = or(image, image2);
+            initializeHistogram((Bitmap)ptbImage.Image);
+        }
+
+        private Bitmap negativeImage(Bitmap image)
+        {
+            for (int w = 0; w < image.Width; w++)
+            {
+                for (int h = 0; h < image.Height; h++)
+                {
+                    Color c = image.GetPixel(w, h);
+                    c = Color.FromArgb(255 - c.R, 255 - c.G, 255 - c.B);
+                    image.SetPixel(w, h, c);
+                }
+            }
+            return image;
+        }
+
+        private Bitmap thresholdImage(Bitmap image, int limiar)
+        {
+            for (int w = 0; w < image.Width; w++)
+            {
+                for (int h = 0; h < image.Height; h++)
+                {
+                    Color p = image.GetPixel(w, h);
+                    image.SetPixel(w, h, Color.FromArgb(p.R > limiar ? 255 : 0, p.G > limiar ? 255 : 0,
+                        p.B > limiar ? 255 : 0));
+                }
+            }
+            return image;
+        }
+
+        private Bitmap and(Bitmap image1, Bitmap image2)
+        {
+            Bitmap image = image1;
+            bool[,] cR = new bool[image.Width, image.Height], cG = new bool[image1.Width, image.Height],
+                cB = new bool[image1.Width, image.Height];
+
+            bool[,] cR1 = new bool[image.Width, image.Height], cG1 = new bool[image1.Width, image.Height],
+                cB1 = new bool[image1.Width, image.Height];
+
+            for (int w = 0; w < image1.Width; w++)
+            {
+                for (int h = 0; h < image1.Height; h++)
+                {
+                    Color p = image1.GetPixel(w, h);
+                    Color p1 = image2.GetPixel(w, h);
+
+                    cR[w, h] = p.R == 0 ? false : true;
+                    cR1[w, h] = p1.R == 0 ? false : true;
+
+                    cG[w, h] = p.G == 0 ? false : true;
+                    cG1[w, h] = p1.G == 0 ? false : true;
+
+                    cB[w, h] = p.B == 0 ? false : true;
+                    cB1[w, h] = p1.B == 0 ? false : true;
+                }
+            }
+
+            for (int w = 0; w < image1.Width; w++)
+            {
+                for (int h = 0; h < image1.Height; h++)
+                {
+                    image.SetPixel(w, h, Color.FromArgb(cR[w, h] && cR1[w, h] == true ? 255 : 0,
+                        cG[w, h] && cG1[w, h] == true ? 255 : 0,
+                        cB[w, h] && cB1[w, h] == true ? 255 : 0));
+                }
+            }
+
+            return image;
+        }
+
+        private Bitmap or(Bitmap image1, Bitmap image2)
+        {
+            Bitmap image = image1;
+            bool[,] cR = new bool[image.Width, image.Height], cG = new bool[image1.Width, image.Height],
+                cB = new bool[image1.Width, image.Height];
+
+            bool[,] cR1 = new bool[image.Width, image.Height], cG1 = new bool[image1.Width, image.Height],
+                cB1 = new bool[image1.Width, image.Height];
+
+            for (int w = 0; w < image1.Width; w++)
+            {
+                for (int h = 0; h < image1.Height; h++)
+                {
+                    Color p = image1.GetPixel(w, h);
+                    Color p1 = image2.GetPixel(w, h);
+
+                    cR[w, h] = p.R == 0 ? false : true;
+                    cR1[w, h] = p1.R == 0 ? false : true;
+
+                    cG[w, h] = p.G == 0 ? false : true;
+                    cG1[w, h] = p1.G == 0 ? false : true;
+
+                    cB[w, h] = p.B == 0 ? false : true;
+                    cB1[w, h] = p1.B == 0 ? false : true;
+                }
+            }
+
+            for (int w = 0; w < image1.Width; w++)
+            {
+                for (int h = 0; h < image1.Height; h++)
+                {
+                    image.SetPixel(w, h, Color.FromArgb(cR[w, h] || cR1[w, h] == true ? 255 : 0,
+                        cG[w, h] || cG1[w, h] == true ? 255 : 0,
+                        cB[w, h] || cB1[w, h] == true ? 255 : 0));
+                }
+            }
+
+            return image;
+        }
+
+        private Bitmap add(Bitmap image1, Bitmap image2)
+        {
+            Bitmap image = image1;
+            int[,] cR = new int[image.Width, image.Height], cG = new int[image1.Width, image.Height],
+                cB = new int[image1.Width, image.Height];
+
+            for (int w = 0; w < image.Width; w++)
+            {
+                for (int h = 0; h < image.Height; h++)
+                {
+                    Color p = image1.GetPixel(w, h);
+                    Color p1 = image2.GetPixel(w, h);
+                    cR[w, h] = p.R + p1.R;
+                    cG[w, h] = p.G + p1.G;
+                    cB[w, h] = p.B + p1.B;
+                }
+            }
+
+            int maxR = highestValue(cR, image);
+            int maxG = highestValue(cG, image);
+            int maxB = highestValue(cB, image);
+            int minR = lowerValue(cR, image);
+            int minG = lowerValue(cG, image);
+            int minB = lowerValue(cB, image);
+
+            for (int w = 0; w < image.Width; w++)
+            {
+                for (int h = 0; h < image.Height; h++)
+                {
+                    cR[w, h] = Escalonate(cR[w, h], maxR, minR);
+                    cG[w, h] = Escalonate(cG[w, h], maxG, minG);
+                    cB[w, h] = Escalonate(cB[w, h], maxB, minB);
+                }
+            }
+
+            for (int w = 0; w < image.Width; w++)
+            {
+                for (int h = 0; h < image.Height; h++)
+                {
+                    image.SetPixel(w, h, Color.FromArgb(cR[w, h], cG[w, h], cB[w, h]));
+                }
+            }
+            return image;
+        }
+
+        private Bitmap subtract(Bitmap image1, Bitmap image2)
+        {
+            Bitmap image = image1;
+            int[,] cR = new int[image.Width, image.Height], cG = new int[image1.Width, image.Height],
+                cB = new int[image1.Width, image.Height];
+
+            for (int w = 0; w < image.Width; w++)
+            {
+                for (int h = 0; h < image.Height; h++)
+                {
+                    Color p = image1.GetPixel(w, h);
+                    Color p1 = image2.GetPixel(w, h);
+                    cR[w, h] = p.R - p1.R;
+                    cG[w, h] = p.G - p1.G;
+                    cB[w, h] = p.B - p1.B;
+                }
+            }
+
+            int maxR = highestValue(cR, image);
+            int maxG = highestValue(cG, image);
+            int maxB = highestValue(cB, image);
+            int minR = lowerValue(cR, image);
+            int minG = lowerValue(cG, image);
+            int minB = lowerValue(cB, image);
+
+            for (int w = 0; w < image.Width; w++)
+            {
+                for (int h = 0; h < image.Height; h++)
+                {
+                    cR[w, h] = Escalonate(cR[w, h], maxR, minR);
+                    cG[w, h] = Escalonate(cG[w, h], maxG, minG);
+                    cB[w, h] = Escalonate(cB[w, h], maxB, minB);
+                }
+            }
+
+            for (int w = 0; w < image.Width; w++)
+            {
+                for (int h = 0; h < image.Height; h++)
+                {
+                    image.SetPixel(w, h, Color.FromArgb(cR[w, h], cG[w, h], cB[w, h]));
+                }
+            }
+            return image;
+        }
+
+        private Bitmap multiplication(Bitmap image1, Bitmap image2)
+        {
+            Bitmap image = image1;
+            int[,] cR = new int[image.Width, image.Height], cG = new int[image1.Width, image.Height],
+                cB = new int[image1.Width, image.Height];
+
+            for (int w = 0; w < image.Width; w++)
+            {
+                for (int h = 0; h < image.Height; h++)
+                {
+                    Color p = image1.GetPixel(w, h);
+                    Color p1 = image2.GetPixel(w, h);
+                    cR[w, h] = p.R * p1.R;
+                    cG[w, h] = p.G * p1.G;
+                    cB[w, h] = p.B * p1.B;
+                }
+            }
+
+            int maxR = highestValue(cR, image);
+            int maxG = highestValue(cG, image);
+            int maxB = highestValue(cB, image);
+            int minR = lowerValue(cR, image);
+            int minG = lowerValue(cG, image);
+            int minB = lowerValue(cB, image);
+
+            for (int w = 0; w < image.Width; w++)
+            {
+                for (int h = 0; h < image.Height; h++)
+                {
+                    cR[w, h] = Escalonate(cR[w, h], maxR, minR);
+                    cG[w, h] = Escalonate(cG[w, h], maxG, minG);
+                    cB[w, h] = Escalonate(cB[w, h], maxB, minB);
+                }
+            }
+
+            for (int w = 0; w < image.Width; w++)
+            {
+                for (int h = 0; h < image.Height; h++)
+                {
+                    image.SetPixel(w, h, Color.FromArgb(cR[w, h], cG[w, h], cB[w, h]));
+                }
+            }
+            return image;
+        }
+
+        private Bitmap division(Bitmap image1, Bitmap image2)
+        {
+            Bitmap image = image1;
+            int[,] cR = new int[image.Width, image.Height], cG = new int[image1.Width, image.Height],
+                cB = new int[image1.Width, image.Height];
+
+            for (int w = 0; w < image.Width; w++)
+            {
+                for (int h = 0; h < image.Height; h++)
+                {
+                    Color p = image1.GetPixel(w, h);
+                    Color p1 = image2.GetPixel(w, h);
+
+                    if (p.R > 0 && p1.R > 0)
+                        cR[w, h] = p.R / p1.R;
+
+                    if (p.G > 0 && p1.G > 0)
+                        cG[w, h] = p.G / p1.G;
+
+                    if (p.B > 0 && p1.B > 0)
+                        cB[w, h] = p.B / p1.B;
+                }
+            }
+
+            int maxR = highestValue(cR, image);
+            int maxG = highestValue(cG, image);
+            int maxB = highestValue(cB, image);
+            int minR = lowerValue(cR, image);
+            int minG = lowerValue(cG, image);
+            int minB = lowerValue(cB, image);
+
+            for (int w = 0; w < image.Width; w++)
+            {
+                for (int h = 0; h < image.Height; h++)
+                {
+                    cR[w, h] = Escalonate(cR[w, h], maxR, minR);
+                    cG[w, h] = Escalonate(cG[w, h], maxG, minG);
+                    cB[w, h] = Escalonate(cB[w, h], maxB, minB);
+                }
+            }
+
+            for (int w = 0; w < image.Width; w++)
+            {
+                for (int h = 0; h < image.Height; h++)
+                {
+                    image.SetPixel(w, h, Color.FromArgb(cR[w, h], cG[w, h], cB[w, h]));
+                }
+            }
+            return image;
+        }
+
+        private int Escalonate(int value, int tmax, int tmin)
+        {
+            double M = 255;
+
+            int r = (int)((M / (tmax - tmin)) * (value - tmin));
+
+            return r;
+        }
+
+        private int lowerValue(int[,] value, Bitmap image)
+        {
+            int tmin = 255;
+
+            for (int w = 0; w < image.Width; w++)
+            {
+                for (int h = 0; h < image.Height; h++)
+                {
+                    if (value[w, h] < tmin)
+                        tmin = value[w, h];
+                }
+            }
+
+            return tmin;
+        }
+
+        private int highestValue(int[,] value, Bitmap image)
+        {
+            int tmax = 0;
+
+            for (int w = 0; w < image.Width; w++)
+            {
+                for (int h = 0; h < image.Height; h++)
+                {
+                    if (value[w, h] > tmax)
+                        tmax = value[w, h];
+                }
+            }
+
+            return tmax;
+        }
+
+        int[,] histoAcumulado;
+        private void initializeHistogram(Bitmap image)
+        {
+            histoAcumulado = histogramaAcumulado(image);
+            //Ejecutamos el botón del histograma rojo
+            histogramRed();
+            histogramGreen();
+            histogramBlue();
+        }
+
+        public int[,] histogramaAcumulado(Bitmap bmp)
+        {
+            
+            byte Red = 0;
+            byte Green = 0;
+            byte Blue = 0;
+            
+            int[,] matrizAcumulada = new int[3, 256];
+            
+            for (int i = 0; i <= bmp.Width - 1; i++)
+            {
+                for (int j = 0; j <= bmp.Height - 1; j++)
+                {
+                    Red = bmp.GetPixel(i, j).R;
+                    
+                    Green = bmp.GetPixel(i, j).G;
+                    Blue = bmp.GetPixel(i, j).B;
+                    
+                    matrizAcumulada[0, Red] += 1;
+                    matrizAcumulada[1, Green] += 1;
+                    matrizAcumulada[2, Blue] += 1;
+                }
+            }
+            return matrizAcumulada;
+        }
+
+        public void histogramRed()
+        {
+            chart1.Series["Histogram R"].Points.Clear();
+            
+            chart1.Series["Histogram R"].Color = Color.Red;
+            for (int i = 0; i <= 255; i++)
+            {
+                chart1.Series["Histogram R"].Points.AddXY(i + 1, histoAcumulado[0, i]);
+            }
+        }
+
+        public void histogramGreen()
+        {
+            chart2.Series["Histogram G"].Points.Clear();
+            chart2.Series["Histogram G"].Color = Color.Green;
+            for (int i = 0; i <= 255; i++)
+            {
+                chart2.Series["Histogram G"].Points.AddXY(i + 1, histoAcumulado[1, i]);
+            }
+        }
+
+        public void histogramBlue()
+        {
+            chart3.Series["Histogram B"].Points.Clear();
+            chart3.Series["Histogram B"].Color = Color.Blue;
+            for (int i = 0; i <= 255; i++)
+            {
+                chart3.Series["Histogram B"].Points.AddXY(i + 1, histoAcumulado[2, i]);
+            }
+        }
+    }
+}
